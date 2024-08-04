6c78cf72c0f83c16a150ab29fa5bed347ce52b20
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-04 15:53:47 +0600

    use "use" keyword to use sub module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod human;
+
 fn main() {
-    println!("Hello, world!");
+    let human = human::Human::new("John".to_string(), 30);
+    println!("Age: {}", human.get_age());
+    println!("Name: {}", human.get_name());
+    
+    let child = human::Child::new("Jane".to_string(), 5);
+    println!("Age: {}", child.get_age());
+    println!("Name: {}", child.get_name());
 }
